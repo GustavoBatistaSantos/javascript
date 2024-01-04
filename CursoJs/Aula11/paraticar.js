0430ef8918be3bdd05997002fb9b56a22f563517
gustavo1506 <145714578+gustavo1506@users.noreply.github.com>
2024-01-04 17:33:53 -0300

    al

---
+++ CursoJs/Aula11/paraticar.js
@@ -0,0 +1,6 @@
+var vel = 53
+console.log(`A velocidade do carro é ${vel}Km/h`)
+if(vel>=60){
+    console.log(`Você ultrapssou a velocidade permitida. Você sera multado`)
+}
+console.log(`Tenha uma boa viagem`)
