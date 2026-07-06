7fb1018d53771c6dca180e281e4a6979279b2185
Gustavo Batista <145714578+gustavo1506@users.noreply.github.com>
2026-07-06 14:36:32 -0300

    jdfksd

---
+++ CursoJs/Aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 71
+console.log(`A velocidade do seu carro ${vel} km/h`)
+if(vel> 60){
+    console.log('Você ultrpassou a velocidade permetida. Multado')
+}
+console.log('Dirija com segurança')
---
+++ CursoJs/Aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'brasil'
+if (pais=='Brasil'){
+    console.log('brasileiro')
+}
+else{
+    console.log('estrangeiro')
+}
